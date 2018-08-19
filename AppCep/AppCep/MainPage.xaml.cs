6e7293183192c6402f702fbe1930b8350343adbc
Rodrigo Avelino <roavelino@icloud.com>
2018-08-19 20:26:25 -0300

    Second commit

--- AppCep/AppCep/MainPage.xaml.cs
+++ AppCep/AppCep/MainPage.xaml.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using AppCep.Servico;
+using AppCep.Servico.Modelo;
+
 
 namespace AppCep
 {
@@ -12,6 +15,64 @@ namespace AppCep
         public MainPage()
         {
             InitializeComponent();
+
+            Btn_send.Clicked += BuscarCep;
         }
+
+        private void BuscarCep(object sender, EventArgs args)
+        {
+            //validate
+
+            //logica
+
+            //search
+            string cep = InputCep.Text.Trim();
+            cep = cep.Replace("-", "");
+
+            if (IsValidCep(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCepServico.BuscarCep(cep);
+                    if (end != null)
+                        lbl_result.Text = string.Format("Endereço: {2}, {3}, {0} , {1}, {2}", end.localidade, end.uf, end.logradouro, end.bairro);
+                    else
+                    DisplayAlert("ERRO", "CEP não encontrado, verifique o CEP e tente novamente", "OK");
+                }
+                catch (Exception ex)
+                {
+
+                    DisplayAlert("ERRO", "O serviço de busca está indisponível./nCódigo do Erro:"+ex.Message, "OK");
+                }
+            }
+            else
+            {
+
+                lbl_result.Text = "O CEP digitado é inválido.";
+
+            }
+
+        }
+
+        private bool IsValidCep(string cep)
+        {
+            int auxCep = 0;
+            if (!int.TryParse(cep, out auxCep))
+            {
+                DisplayAlert("ERRO", "O cep digitado contém letras.", "OK");
+                return false;
+            }
+
+            if (cep.Length != 8)
+            {
+                DisplayAlert("ERRO", "O cep digitado está incompleto ou possui números a mais.", "OK");
+                return false;
+            }
+           
+                       
+                return true;
+            
+        }
+
     }
 }
---
+++ AppCep/AppCep/Service/ViaCepServico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using AppCep.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace AppCep.Servico
+{
+    public class ViaCepServico
+    {
+        private static string cepUrl = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarCep(string cep)
+        {
+            string novaUrl = string.Format(cepUrl, cep);
+            WebClient wc = new WebClient();
+            string content = wc.DownloadString(novaUrl);
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(content);
+            if (end.cep == null) return null;
+            return end;            
+        }
+    }
+}
